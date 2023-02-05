82a59be24821bc8eff93e162d0b0818685556df1
Kartikay Kapil <77982430+kartikay23@users.noreply.github.com>
2023-02-05 22:34:13 +0530

    changes made

---
+++ server/utils/createTokenUser.js
@@ -0,0 +1,8 @@
+const createTokenUser = (user) => {
+    return {
+      userId: user.user_id,
+      username: user.username,
+      role: user.role,
+    };
+  };
+  module.exports = createTokenUser;
