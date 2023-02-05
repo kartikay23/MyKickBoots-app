1f3d305d0016823dd9ca5bc0d747e53129d66ce7
Kartikay Kapil <77982430+kartikay23@users.noreply.github.com>
2023-02-05 22:35:17 +0530

    changes made to backend

---
+++ server/app.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware imports
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleWare = require("./middleware/error-handler");
+
+// Routes imports
+const authRouter = require("./routes/authRoutes");
+const userRouter = require("./routes/userRoutes");
+const productRouter = require("./routes/productRoutes");
+const reviewRouter = require("./routes/reviewRoutes");
+const orderRouter = require("./routes/orderRoutes");
+
+const connectDB = require("./db/connect");
+
+const corsOptions = {
+  origin: "http://localhost:5000",
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser(process.env.SIGNED_COOKIE_SECRET));
+
+app.get("/api", async (req, res) => {
+  res.status(200).send("API is working.");
+});
+
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.use("/api/reviews", reviewRouter);
+app.use("/api/orders", orderRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleWare);
+
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, console.log(`Server running on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
